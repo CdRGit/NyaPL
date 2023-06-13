fbc6330f1d8648a596b5371326dc31bc11993a43
CdRGit <github@catelyn.tech>
2023-06-13 20:52:45 +0200

    changed the way FlowAnalyzer.cs handles multiple errors to make code less cumbersome

--- csharp_compiler/FlowAnalysis/FlowAnalyzer.cs
+++ csharp_compiler/FlowAnalysis/FlowAnalyzer.cs
@@ -6,13 +6,13 @@ using Nyapl.Parsing.Tree;
 namespace Nyapl.FlowAnalysis;
 
 public class FlowAnalyzer {
-	private IEnumerable<FlowError> Analyze(Context ctx, AstListNode<StatementNode> body) {
+	private void Analyze(Context ctx, AstListNode<StatementNode> body) {
 		bool reportedDeadCode = false;
 
 		foreach (var statement in body) {
 			if (!reportedDeadCode && statement is not FunctionNode && ctx.Returns) {
 				// report dead code (this may move to a different phase of the compiler later on)
-				yield return new FlowError(statement.Location, $"Dead code");
+				ctx.Errors.Add(new FlowError(statement.Location, $"Dead code"));
 				reportedDeadCode = true;
 			}
 			// for now I can just analyze all the statements in here, most do not require a lot of logic
@@ -20,7 +20,7 @@ public class FlowAnalyzer {
 				// does not impact control flow but does need to be recursively checked
 				case FunctionNode function:
 					bool returnsCopy = ctx.Returns;
-					foreach (var error in Analyze(ctx, function)) yield return error;
+					Analyze(ctx, function);
 					ctx.Returns = returnsCopy;
 					break;
 
@@ -38,34 +38,34 @@ public class FlowAnalyzer {
 		// done!
 	}
 
-	private IEnumerable<FlowError> Analyze(Context ctx, FunctionNode function) {
+	private void Analyze(Context ctx, FunctionNode function) {
 		ctx.Returns = false; // assume no paths return
 
-		foreach (var error in Analyze(ctx, function.Body)) yield return error;
+		Analyze(ctx, function.Body);
 
 		// add a code path flow error to the list if not all paths return, so code path return errors can be reported in bulk
-		if (!ctx.Returns) yield return new FlowError(function.Location, "Not all code paths return");
+		if (!ctx.Returns) ctx.Errors.Add(new FlowError(function.Location, "Not all code paths return"));
 	}
 
 	public FileNode Analyze(FileNode file) {
 		var ctx = new Context();
-		// keep track of multiple errors so I can bulk report them
-		List<CompileError> errors = new();
-		
 		// the only things in a file with flow currently are Functions, which (currently) only have one flow path, will still write the code to theoretically support multiple flow paths, might need to rewrite parts of this
 		foreach (var function in file.Functions) {
-			errors.AddRange(Analyze(ctx, function));
+			Analyze(ctx, function);
 		}
 
-		if (errors.Count > 0) {
-			throw new BundledError(errors.AsReadOnly());
+		// bulk report errors
+		if (ctx.Errors.Count > 0) {
+			throw new BundledError(ctx.Errors.AsReadOnly());
 		}
 
 		return file;
 	}
 
 	private class Context {
-		// in some cases not all paths having a return is fine, for instance for `if` (in the future) and other branching control flow having only some branches return is fine
+		// in some cases not all paths having a return is fine, for instance for `if` (in the future) and other branching control flow having only some branches return is fine, so we use a boolean rather than directly reporting errors
 		public bool Returns { get; set; }
+		// store a list of errors in context to allow seperate analysis to add errors without needing to pass those down
+		public List<CompileError> Errors { get; set; } = new();
 	}
 }
