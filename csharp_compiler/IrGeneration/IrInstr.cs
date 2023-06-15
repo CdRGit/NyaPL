4628637566b5ad2e06fd36f09003ac6ffe2f1533
CdRGit <github@catelyn.tech>
2023-06-15 20:30:08 +0200

    modified IrInstr.ToString() to make printout more usable for me

--- csharp_compiler/IrGeneration/IrInstr.cs
+++ csharp_compiler/IrGeneration/IrInstr.cs
@@ -13,7 +13,13 @@ public readonly struct IrInstr {
 		Param2 = param2;
 	}
 
-	public override string ToString() => $"{Kind} {Param0} {Param1} {Param2}";
+	private static string ParamToString(ulong param) {
+		string tostr = $"{param:X16}";
+		return tostr.Substring(0, 4) + ":" + tostr.Substring(4, 4) + ":" + tostr.Substring(8, 4) + ":" + tostr.Substring(12, 4);
+
+	}
+
+	public override string ToString() => $"{Kind,15} {ParamToString(Param0)} {ParamToString(Param1)} {ParamToString(Param2)}";
 
 	public enum IrKind {
 		Return,
