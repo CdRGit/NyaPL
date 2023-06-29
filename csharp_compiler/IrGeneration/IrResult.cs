c4fd7951fa1dca69cfad63fcd5042c71bc7fb39b
CdRGit <github@catelyn.tech>
2023-06-29 20:17:24 +0200

    renamed IrList to IrResult

--- csharp_compiler/IrGeneration/IrResult.cs
+++ csharp_compiler/IrGeneration/IrResult.cs
@@ -4,12 +4,12 @@ using Nyapl.Localizing;
 
 namespace Nyapl.IrGeneration;
 
-public class IrList {
+public class IrResult {
 	public Localizer.Platform Platform { get; }
 	public ReadOnlyCollection<IrInstr> Instructions { get; }
 	public ReadOnlyDictionary<string, int> Functions { get; }
 
-	public IrList(Localizer.Platform platform, ReadOnlyCollection<IrInstr> instructions, ReadOnlyDictionary<string, int> functions) {
+	public IrResult(Localizer.Platform platform, ReadOnlyCollection<IrInstr> instructions, ReadOnlyDictionary<string, int> functions) {
 		Platform = platform;
 		Instructions = instructions;
 		Functions = functions;
