12b02281151a1c9f081b4b0ab9a659d1b7eee7ea
CdRGit <github@catelyn.tech>
2023-08-23 23:21:40 +0200

    start generating NASM asm

--- csharp_compiler/CodeGen/CodeGenLinux_x86_64.cs
+++ csharp_compiler/CodeGen/CodeGenLinux_x86_64.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.ObjectModel;
 
 using Nyapl.IrGeneration;
@@ -7,6 +8,10 @@ namespace Nyapl.CodeGen;
 
 public class CodeGenLinux_x86_64 : ICodeGen {
 	public void Generate(string filePath, ReadOnlyDictionary<string, IrBlock> functions) {
+		using (StreamWriter asmWriter = new($"{filePath}.asm")) {
+			asmWriter.WriteLine(".text");
+			asmWriter.WriteLine(".global _start");
+		}
 		throw new Exception("TODO!");
 	}
 }
