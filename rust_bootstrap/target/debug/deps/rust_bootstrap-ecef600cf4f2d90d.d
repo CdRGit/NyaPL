37877aaf1938eb644f0d94e592417ede3b526725
CdRGit <github@catelyn.tech>
2024-01-04 15:20:04 +0100

    rust rewrite begin

---
+++ rust_bootstrap/target/debug/deps/rust_bootstrap-ecef600cf4f2d90d.d
@@ -0,0 +1,5 @@
+/media/data/Projects/PLDev/nyapl/rust_bootstrap/target/debug/deps/rust_bootstrap-ecef600cf4f2d90d: src/main.rs
+
+/media/data/Projects/PLDev/nyapl/rust_bootstrap/target/debug/deps/rust_bootstrap-ecef600cf4f2d90d.d: src/main.rs
+
+src/main.rs:
---
+++ rust_bootstrap/target/debug/rust_bootstrap.d
@@ -0,0 +1,1 @@
+/media/data/Projects/PLDev/nyapl/rust_bootstrap/target/debug/rust_bootstrap: /media/data/Projects/PLDev/nyapl/rust_bootstrap/src/main.rs
